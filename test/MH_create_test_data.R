d54e9be5cd85946487170a39e1ad5c7e3eb30914
Sarina Atkinson <71387505+SarinaAtkinson-NOAA@users.noreply.github.com>
2022-12-09 15:33:04 -0500

    Use all fields of analysis ready as the test data set #10

--- test/MH_create_test_data.R
+++ test/MH_create_test_data.R
@@ -5,13 +5,7 @@
   # Recreational size limits for South Atlantic gray triggerfish = cluster 952 - has different zones
   # Commercial size limits for South Atlantic red snapper = cluster 365 - has reg removed flag
 
-test <- mh_expanded %>% filter(CLUSTER %in% c(952, 365)) %>%
-  select(CLUSTER, REGULATION_ID, FR_CITATION,
-         MANAGEMENT_TYPE_USE, SECTOR_USE, SUBSECTOR_USE, ZONE_USE, SPP_TYPE, SPP_NAME, MANAGEMENT_STATUS_USE,
-         REG_REMOVED, ADJUSTMENT, MULTI_REG,
-         EFFECTIVE_DATE, INEFFECTIVE_DATE,
-         START_DATE2, START_TIME_USE, END_DATE, END_TIME_USE,
-         VALUE, VALUE_UNITS, VALUE_TYPE, VALUE_RATE) %>%
+test <- mh_analysis_ready %>% filter(CLUSTER %in% c(952, 365)) %>%
   arrange(CLUSTER, ZONE_USE, START_DATE2)
 
 # Keep only test data set in R Environment work space
