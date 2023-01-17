0396261006d63cb45764b578b24532097cc4edf9
Sarina Atkinson <71387505+SarinaAtkinson-NOAA@users.noreply.github.com>
2023-01-17 14:32:03 -0500

    Add most recent MH csv file name

--- code/MH00_data_formatting.R
+++ code/MH00_data_formatting.R
@@ -2,7 +2,7 @@
 # Data reformatting for R
 
 # Read in data
-mh <- read.csv(here('data/raw', "MH_DOWNLOAD_DEC_30_2022.csv"), 
+mh <- read.csv(here('data/raw', "MH_DOWNLOAD_JAN_13_2023.csv"), 
                stringsAsFactors = FALSE,
                colClasses=c("REGULATION_ID" = "numeric",
                             "START_DAY" = "numeric",
