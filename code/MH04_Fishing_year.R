23119cb67c6889903ffe1509af8509187dfcc945
Sarina Atkinson <71387505+SarinaAtkinson-NOAA@users.noreply.github.com>
2023-01-20 18:12:39 -0500

    #15 start processing fishing season, but needs more thought. Recoded fishing season to fishing year if 365 days

--- code/MH04_Fishing_year.R
+++ code/MH04_Fishing_year.R
@@ -1,8 +1,49 @@
+# Process fishing year and fishing season
+
+# FISHING SEASON MH PROCESSING -- NOT DONE, STILL NEED SOME THOUGHT
+
+# Filter for only fishing season management type
+# Include REG_REMOVED == 0 because this helps us in getting an end date for fishing years that end, but we do not want to keep the record that "turns off" the regulation
+mh_fs <- mh_cluster_ids %>%
+  filter(MANAGEMENT_TYPE_USE == 'FISHING SEASON', REG_REMOVED == 0)
+
+# Identify cases that have a split fishing season - yes, we have split fishing season 
+mh_fs_chk1 <- mh_fs %>% group_by(FR_CITATION, FMP, REGION, SPP_NAME, SECTOR_USE, SUBSECTOR_USE, ZONE_USE, EFFECTIVE_DATE) %>%
+  summarise(N_seasons = n()) 
+# Join to data
+mh_fs2 <- mh_fs %>% 
+  left_join(mh_fs_chk1, by = c("FR_CITATION", "REGION", "FMP", "EFFECTIVE_DATE", "SECTOR_USE", "SPP_NAME", "ZONE_USE",
+                               "SUBSECTOR_USE"))
+
+# Identify number of days within a fishing season 
+mh_fs_ck2 <- mh_fs2 %>% 
+  # Create as date field to subtract (the year is not important here)
+  mutate(START_FY = case_when(!is.na(START_MONTH) & !is.na(START_DAY_USE) & is.na(START_YEAR) ~ as.Date(paste0("2023-", START_MONTH, "-", START_DAY_USE), "%Y-%m-%d"),
+                              !is.na(START_MONTH) & !is.na(START_DAY_USE) & !is.na(START_YEAR) ~ as.Date(paste0(START_YEAR, "-", START_MONTH, "-", START_DAY_USE), "%Y-%m-%d")),
+         END_FY = case_when(!is.na(END_MONTH_USE) & !is.na(END_DAY_USE) & is.na(END_YEAR) ~ as.Date(paste0("2023-", END_MONTH_USE, "-", END_DAY_USE), "%Y-%m-%d"),
+                            !is.na(END_MONTH_USE) & !is.na(END_DAY_USE) & !is.na(END_YEAR) ~ as.Date(paste0(END_YEAR, "-", END_MONTH_USE, "-", END_DAY_USE), "%Y-%m-%d")),
+         DIFF = case_when(END_MONTH_USE >= START_MONTH ~ END_FY - START_FY,
+                          END_MONTH_USE < START_MONTH ~ START_FY - END_FY)) %>%
+  filter(N_seasons == 1)
+
+# Fishing season more than 365 days
+fs_gt365 <- mh_fs_ck2 %>%
+  filter(abs(DIFF) > 365) %>% select(REGULATION_ID, FR_CITATION, FR_URL) %>% distinct()
+
+# Recode fishing season records as fishing year if 365 days
+fs_to_fy <- mh_fs_ck2 %>%
+  filter(DIFF == -364 | DIFF == -365 | DIFF == 1) %>%
+  select(REGULATION_ID) %>% pull()
+
+# All other fishing seasons < 365 days need to add closure for other days?
+
 # FISHING YEAR MH PROCESSING
 
 # Filter for only fishing year management type
 # Include REG_REMOVED == 0 because this helps us in getting an end date for fishing years that end, but we do not want to keep the record that "turns off" the regulation
 mh_fy <- mh_cluster_ids %>%
+  mutate(MANAGEMENT_TYPE_USE = case_when(REGULATION_ID %in% fs_to_fy ~ 'FISHING YEAR',
+                                         TRUE ~ MANAGEMENT_TYPE_USE)) %>%
   filter(MANAGEMENT_TYPE_USE == 'FISHING YEAR', REG_REMOVED == 0)
 
 # Identify clusters that have a split fishing year - NONE
@@ -55,23 +96,4 @@ mh_fy_w <- mh_fy3 %>%
 # CHECK we have two fishing years for All CMP species for recreational sector
 chk <- mh_fy_w %>% filter(FMP == 'COASTAL MIGRATORY PELAGIC RESOURCES', REGION == 'GULF OF MEXICO', SECTOR_USE == 'RECREATIONAL', SPP_NAME == 'ALL')
 
-# FISHING SEASON MH PROCESSING -- NOT DONE, STILL NEED SOME THOUGHT
-
-# Filter for only fishing season management type
-# Include REG_REMOVED == 0 because this helps us in getting an end date for fishing years that end, but we do not want to keep the record that "turns off" the regulation
-mh_fs <- mh_cluster_ids %>%
-  filter(MANAGEMENT_TYPE_USE == 'FISHING SEASON', REG_REMOVED == 0)
-
-# Identify cases that have a split fishing season - yes, we have split fishing season 
-mh_fs2 <- mh_fs %>% group_by(FR_CITATION, FMP, REGION, SPP_NAME, SECTOR_USE, SUBSECTOR_USE, ZONE_USE, EFFECTIVE_DATE) %>%
-  summarise(N_seasons = n()) 
-# Join to data
-mh_fs3 <- mh_fs %>% 
-  left_join(mh_fs2, by = c("FR_CITATION", "REGION", "FMP", "EFFECTIVE_DATE", "SECTOR_USE", "SPP_NAME", "ZONE_USE",
-                           "SUBSECTOR_USE"))
-
-# Identify records with a fishing season that is less than 1 year and not a split fishing season
-mh_fs4 <- mh_fs3 %>% 
-  select(FMP, REGION, SECTOR_USE, SUBSECTOR_USE, SPP_NAME, ZONE_USE, EFFECTIVE_DATE, START_MONTH, START_DAY, START_YEAR, START_TIME,
-         INEFFECTIVE_DATE, END_MONTH, END_DAY, END_YEAR, END_TIME, N_seasons)
-
+# Add a reopening to each closure cluster at the start of the fishing season or fishing year
