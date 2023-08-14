4cc6929fe1fc54fb05301643911985080db93196
Adyan Rios <87143630+AdyanRios-NOAA@users.noreply.github.com>
2023-08-14 12:48:53 -0400

    Added more context to draft species specific ciation extractions

--- Examples/pull_citations/pull_citations.R
+++ Examples/pull_citations/pull_citations.R
@@ -22,16 +22,22 @@ list_citations <- mh_data_log |>
   ungroup() |>
   dplyr::filter(SPECIES_ITIS_USE == itis,
                 REGION == region) |>
-  dplyr::select(FR_CITATION) |>
-  distinct()
+  unite(col = "FR_ACTION",
+        ACTION, ACTION_TYPE, AMENDMENT_NUMBER, na.rm=TRUE, sep = ", ") |>
+  dplyr::select(FR_CITATION, FR_ACTION) |>
+  distinct() |>
+  arrange(FR_CITATION)
 
 # FILTER TO A LIST OF UNIQUE REGULATION TYPES AND FR CITATIONS
 list_reg_types <- mh_data_log |>
   ungroup() |>
   dplyr::filter(SPECIES_ITIS_USE == itis,
                 REGION == region) |>
-  dplyr::select(FR_CITATION, MANAGEMENT_TYPE_USE) |>
-  distinct()
+  unite(col = "FR_ACTION",
+        ACTION, ACTION_TYPE, AMENDMENT_NUMBER, na.rm=TRUE, sep = ", ") |>
+  dplyr::select(FR_CITATION, FR_ACTION, MANAGEMENT_TYPE_USE, ZONE_USE, SPP_NAME) |>
+  distinct() |>
+  arrange(FR_CITATION)
 
 # SAVE SPECIFIC LIST OF FR CITATIONS and MANAGMENT TYPES
-write.csv(list_reg_types, here("Examples", "pull_citations", paste0(short_name, "_", format(Sys.Date(), "%Y%b%d"), ".csv")))
+write.csv(list_reg_types, here("Examples", "pull_citations", paste0(short_name, "_", format(Sys.Date(), "%Y%b%d"), ".csv")), row.names = FALSE)
