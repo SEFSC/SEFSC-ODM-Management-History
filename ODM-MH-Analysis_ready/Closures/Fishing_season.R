f348115b9945eb6c3fde9de7836d2bd06a4191b0
Sarina Atkinson <sarina.atkinson@noaa.gov>
2023-09-11 12:14:52 -0400

    Move fishing season script to Closures folder

--- ODM-MH-Analysis_ready/Closures/Fishing_season.R
+++ ODM-MH-Analysis_ready/Closures/Fishing_season.R
@@ -6,7 +6,7 @@
 librarian::shelf(here, tidyverse)
 
 # Read in MH Data Log
-mh_data_log <- readRDS(here("ODM-MH-Data_log", "data", "results", "MH_AL_2023Aug15.RDS"))
+mh_data_log <- readRDS(here("ODM-MH-Analysis_ready", "results", "MH_DL_wFY_2023Sep08.RDS"))
 
 # Filter for only fishing season management type
 # Include REG_REMOVED == 0 because this helps us in getting an end date for fishing years that end, but we do not want to keep the record that "turns off" the regulation
@@ -14,12 +14,40 @@ mh_fs <- mh_data_log %>%
   filter(MANAGEMENT_TYPE_USE == 'FISHING SEASON', REG_REMOVED == 0)
 
 # Identify cases that have a split fishing season - yes, we have split fishing season 
-mh_fs_chk1 <- mh_fs %>% group_by(FR_CITATION, FMP, REGION, SPP_NAME, SECTOR_USE, SUBSECTOR_USE, ZONE_USE, EFFECTIVE_DATE) %>%
+mh_fs_chk1 <- mh_fs %>% ungroup() %>%
+  group_by(FR_CITATION, FMP, REGION, COMMON_NAME_USE, SECTOR_USE, SUBSECTOR_USE, ZONE_USE, EFFECTIVE_DATE) %>%
   summarise(N_seasons = n()) 
 # Join to data
 mh_fs2 <- mh_fs %>% 
-  left_join(mh_fs_chk1, by = c("FR_CITATION", "REGION", "FMP", "EFFECTIVE_DATE", "SECTOR_USE", "SPP_NAME", "ZONE_USE",
-                               "SUBSECTOR_USE"))
+  left_join(mh_fs_chk1, by = c("FR_CITATION", "REGION", "FMP", "EFFECTIVE_DATE", "SECTOR_USE", 
+                               "COMMON_NAME_USE", "ZONE_USE", "SUBSECTOR_USE"))
+
+# Check spiny lobster - had no fishing year because has fishing season?
+# Include fishing season in "CLOSURE" story and process there
+chk <- mh_data_log %>% 
+  filter(COMMON_NAME_USE == 'LOBSTER, CARIBBEAN SPINY', is.na(FY_1),
+         MANAGEMENT_TYPE_USE == 'FISHING SEASON') %>%
+  select(CLUSTER, FR_CITATION, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, COMMON_NAME_USE,
+         FMP, REGION, SECTOR_USE, SUBSECTOR_USE, ZONE_USE,
+         EFFECTIVE_DATE, INEFFECTIVE_DATE, START_DATE2, END_DATE2, 
+         START_MONTH, START_DAY_USE, START_DAY_OF_WEEK_USE, START_TIME_USE, START_YEAR,
+         END_MONTH_USE, END_DAY_USE, END_DAY_OF_WEEK_USE, END_TIME_USE, END_YEAR) %>%
+  arrange(REGION, SECTOR_USE, SUBSECTOR_USE, START_DATE2)
+
+# Check red snapper that has 2 fishing seasons
+chk <- mh_data_log %>% 
+  filter(COMMON_NAME_USE == 'SNAPPER, RED', REGION == 'GULF OF MEXICO',
+         MANAGEMENT_TYPE_USE == 'FISHING SEASON') %>%
+  select(CLUSTER, FR_CITATION, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, COMMON_NAME_USE,
+         FMP, REGION, SECTOR_USE, SUBSECTOR_USE, ZONE_USE,
+         EFFECTIVE_DATE, INEFFECTIVE_DATE, START_DATE2, END_DATE2, 
+         START_MONTH, START_DAY_USE, START_DAY_OF_WEEK_USE, START_TIME_USE, START_YEAR,
+         END_MONTH_USE, END_DAY_USE, END_DAY_OF_WEEK_USE, END_TIME_USE, END_YEAR,
+         EFFECTIVE_DATE_FY_1, EFFECTIVE_DATE_FY_2, EFFECTIVE_DATE_FY_3,
+         FY_1, FY_2, FY_3) %>%
+  arrange(REGION, SECTOR_USE, SUBSECTOR_USE, START_DATE2)
+
+
 
 # Identify number of days within a fishing season 
 mh_fs_ck2 <- mh_fs2 %>% 
@@ -41,4 +69,6 @@ fs_to_fy <- mh_fs_ck2 %>% ungroup() %>%
   filter(DIFF == -364 | DIFF == -365 | DIFF == 1) %>%
   select(REGULATION_ID) %>% pull()
 
-# All other fishing seasons < 365 days need to add closure for other days?
+# Remove these FRs since already added to fishing year
+
+
