c75b3560e17071c273324da5d9b0c0985b3d71c5
Sarina Atkinson <sarina.atkinson@noaa.gov>
2023-10-06 14:43:49 -0400

    Comments from 10/6 meeting

--- ODM-MH-Analysis_ready/Closures/Main_closure_prep.R
+++ ODM-MH-Analysis_ready/Closures/Main_closure_prep.R
@@ -5,16 +5,24 @@
 librarian::shelf(here, tidyverse)
 
 # Read in MH Data Log
-mh_data_log <- readRDS(here("ODM-MH-Analysis_ready", "results", "MH_DL_wFY_2023Sep14.RDS"))
+mh_data_log <- readRDS(here("ODM-MH-Analysis_ready", "results", "MH_DL_wFY_2023Sep15.RDS"))
 
 # Select species and region
 spp <- 'PORGY, RED'
 region <- 'SOUTH ATLANTIC'
 
-# Process fishing season
+start.date
+end.date
+
+# Create empty dataframe of dates
+
+# Process fishing season and expand
 source(here("ODM-MH-Analysis_ready", "Closures", "Fishing_season.R"))
 
-# Process prohibited species or prohibited sale
+# Process prohibited species or prohibited sale and expand
 
-# Process closures and re-openings
+# Process closures and re-openings and expand
 
+# Join everything - overwrite
+
+# Collapse
