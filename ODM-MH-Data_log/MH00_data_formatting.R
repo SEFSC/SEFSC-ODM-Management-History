fe2de4499928a1ea379d9a36cc748e077b8f6448
GaitlynMalone-NOAA <115589280+GaitlynMalone-NOAA@users.noreply.github.com>
2024-02-06 15:03:22 -0500

    Updated ODM download based on removal of Reg ID 764

--- ODM-MH-Data_log/MH00_data_formatting.R
+++ ODM-MH-Data_log/MH00_data_formatting.R
@@ -2,7 +2,7 @@
 # Data reformatting for R
 
 # Read in data
-mh <- read.csv(here('ODM-MH-Data_log', 'data/raw', "MH_DOWNLOAD_FEB_05_2024.csv"), 
+mh <- read.csv(here('ODM-MH-Data_log', 'data/raw', "MH_DOWNLOAD_FEB_06_2024.csv"), 
                stringsAsFactors = FALSE,
                colClasses=c("REGULATION_ID" = "numeric",
                             "START_DAY" = "numeric",
