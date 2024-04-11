b3bfa98c2ac74ab968a22ebbdb38535ec4b882bb
GaitlynMalone-NOAA <115589280+GaitlynMalone-NOAA@users.noreply.github.com>
2024-04-11 15:48:03 -0400

    Update NEVER_IMPLEMENTED flag to not flag cases where a CLOSURE/REOPENING is overwritten for a period of time but the original CLOSURE/REOPENING still remains.

--- ODM-MH-Data_log/MH04_dates.R
+++ ODM-MH-Data_log/MH04_dates.R
@@ -176,6 +176,7 @@ mh_dates <- mh_reversions %>%
          NEVER_IMPLEMENTED = case_when(
            # Added 2/8/24 to remove confusion related to Catch Limit management types
                                       MANAGEMENT_CATEGORY == "CATCH LIMITS" ~ 0,
+                                      MANAGEMENT_CATEGORY == "TEMPORAL CONTROLS" & START_DATE_USE == lag(START_DATE_USE) & END_DATE != lag(END_DATE) & VALUE != lag(VALUE) ~ 0,
                                       MULTI_REG_SEASONAL == 1 ~ 0,
                                       START_DATE_USE > END_DATE ~ 1,
                                        MULTI_REG_VALUE == 1 ~ 0,
